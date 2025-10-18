31760b3d9333540630180e026038ee759e08ff87
arpit <arpit2003y@gmail.com>
2025-10-18 09:48:51 +0530

    chore(changes):minor_changes

--- cli/build.py
+++ cli/build.py
@@ -4,5 +4,7 @@ from lib.indexes.inverted_index import InvertedIndex
 
 if __name__ == "__main__":
     CURRENT_INVERTED_INDEX = InvertedIndex()
-    CURRENT_INVERTED_INDEX.build(load_movie_data())
+    CURRENT_INVERTED_INDEX.build(
+        load_movie_data(),
+    )
     CURRENT_INVERTED_INDEX.save()
